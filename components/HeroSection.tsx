cf398abc4c0349bac9e7e3bfc9aef9c94a6c9b4f
violekdev <71582866+violekdev@users.noreply.github.com>
2024-04-18 17:13:35 +0530

    UI: Added Responsive CSS

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,7 @@ import { TextRevealCard } from "./ui/text-reveal-card";
 
 export default function HeroSection() {
   return (
-    <div className="flex justify-center items-center h-[20rem]">
+    <div className="flex justify-center items-center h-[12rem] sm:h-[20rem] max-w-full px-4 sm:px-8 md:px-12">
       <TextRevealCard
         text="You can't jeet the boss"
         revealText="I can't jeet the boss."
