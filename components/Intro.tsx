c4f6aeba526367e86c7d0c5dfcd150dd333a90ca
violekdev <71582866+violekdev@users.noreply.github.com>
2024-04-20 15:11:35 +0530

    Update Intro.tsx

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,13 +1,11 @@
 "use client";
 import { TextGenerateEffect } from "./ui/text-generate-effect";
 
-const words = `Hi, I am Tolyjeet, I jeet the fuck out of solana shit coins. I
-drink lassi. Pancho friday ah! Oye papa ji! Andi mandi shandi, loda lassan!
-`;
+const words = `Yo, it's ya boy Tolyjeet, the desi Jeet in town! I'm Anatoly's Indian fam, and I'm all about that tunak tunak life and dhincak dhincak vibes! Let's unite and build the Jeet squad on Solana. While our Russian cousin's busy with Solana and Ukraine drama, us South Asians gotta HODL it down! BUY, SPLURGE, and TUNAK TUNAK! And don't jeet because you can't jeet the boss!`;
 
 export default function Intro() {
   return (
-    <div className="text-center text-lg text-balance">
+    <div className="text-center text-lg text-balance sm:w-3/4 ">
       <TextGenerateEffect words={words} />
     </div>
   );
