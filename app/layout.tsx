cbbc9ca1eb39ac0b8202f897d583dcfa4ace4469
Vaibhav Sharma <vaibhavsharma@192.168.1.5>
2024-04-20 22:13:37 +0530

    marquee removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <MarqueeComponent />
+        {/* <MarqueeComponent /> */}
       </body>
     </html>
   );
